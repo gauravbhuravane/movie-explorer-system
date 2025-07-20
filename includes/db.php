28ebb13a1d0cfa52d8a0698916942210cbd3990e
gauravbhuravane <bhuravanegaurav123@gmail.com>
2025-07-20 17:51:30 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASSWORD = "";
-$DB_NAME = "movie_explorer";
+$DB_HOST = getenv('DB_HOST');
+$DB_USER = getenv('DB_USER');
+$DB_PASSWORD = getenv('DB_PASSWORD');
+$DB_NAME = getenv('DB_NAME');
 
 // Create connection
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
